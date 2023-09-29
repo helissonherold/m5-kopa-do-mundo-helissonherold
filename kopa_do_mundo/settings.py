a7b02a22ef2a370efae96180716bd3631bca0a53
unknown <helissonherold@yahoo.com.br>
2023-09-29 16:18:18 -0300

    feat: tarefa_2_finalizada

--- kopa_do_mundo/settings.py
+++ kopa_do_mundo/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "teams",
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "_kopa_do_mundo.urls"
+ROOT_URLCONF = "kopa_do_mundo.urls"
 
 TEMPLATES = [
     {
@@ -67,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "_kopa_do_mundo.wsgi.application"
+WSGI_APPLICATION = "kopa_do_mundo.wsgi.application"
 
 
 # Database
