26c1807e66c177f19cce6c3f7b7839903a5bf504
unknown <helissonherold@yahoo.com.br>
2023-10-03 10:48:15 -0300

    todas_tarefas_finalizada_revisada

--- kopa_do_mundo/urls.py
+++ kopa_do_mundo/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from teams.views import TeamView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
